b6e5179aee1f5f3338777505d09e0bfdefdebbf0
Daniel Leszczyński <daniel.leszczynski@hotmail.com>
2015-02-20 12:12:37 +0100

    path w xmlu wynikowym test

--- BoxerXmlComparator/BoxerXmlComparator/Comparator.cs
+++ BoxerXmlComparator/BoxerXmlComparator/Comparator.cs
@@ -26,7 +26,7 @@ namespace BoxerXmlComparator
             return this.ResultDocument;
         
         }
-
+        
         private void CompareTaggedTokens(XmlDocument OriginXml, XmlDocument CorrectedXml, XmlDocument ResultDocument)
         {
             var OriginTaggedTokensNode = OriginXml.SelectSingleNode("//taggedtokens");
@@ -42,8 +42,6 @@ namespace BoxerXmlComparator
                         var correctedNode = CorrectedTaggedTokensNode.ChildNodes[i].FirstChild.ChildNodes[j];
                         if (node.OuterXml != correctedNode.OuterXml)
                         {
-                            
-
                             XmlNode originalnode = ResultDocument.CreateElement("original");
                             XmlAttribute attribute = ResultDocument.CreateAttribute("line");
                             originalnode.InnerText = node.ParentNode.ParentNode.Name + " " + node.ParentNode.ParentNode.Attributes[0].Name + " "  + node.ParentNode.ParentNode.Attributes[0].Value + " " + node.OuterXml;
@@ -52,6 +50,12 @@ namespace BoxerXmlComparator
                             XmlNode correctednode = ResultDocument.CreateElement("corrected");
                             correctednode.InnerText = correctedNode.ParentNode.ParentNode.Name + " " + correctedNode.ParentNode.ParentNode.Attributes[0].Name + " " + correctedNode.ParentNode.ParentNode.Attributes[0].Value + " " + correctedNode.OuterXml;
                             rootNode.AppendChild(correctednode);
+
+
+                            XmlNode pathnode = ResultDocument.CreateElement("path");
+                            pathnode.InnerText = GetXPathToNode(correctedNode) + " " + pathnode.OuterXml;
+                            rootNode.AppendChild(pathnode);
+
 
                             MessageBox.Show("RIGHT: <" + node.ParentNode.ParentNode.Name + " " + node.ParentNode.ParentNode.Attributes[0].Name + "=" + node.ParentNode.ParentNode.Attributes[0].Value + ">" + node.OuterXml + "\nLEFT: <" + correctedNode.ParentNode.ParentNode.Name + " " + correctedNode.ParentNode.ParentNode.Attributes[0].Name + "=" + correctedNode.ParentNode.ParentNode.Attributes[0].Value + ">" + correctedNode.OuterXml);
                         }
@@ -69,6 +73,45 @@ namespace BoxerXmlComparator
             //{
             //    MessageBox.Show(sectionOriginal.ToString() + " | " + sectionCorrected.ToString());
             //}
+        }
+
+        static int GetNodePosition(XmlNode child)
+        {
+            for (int i = 0; i < child.ParentNode.ChildNodes.Count; i++)
+            {
+                if (child.ParentNode.ChildNodes[i] == child)
+                {
+                    // tricksy XPath, not starting its positions at 0 like a normal language
+                    return i + 1;
+                }
+            }
+            throw new InvalidOperationException("Child node somehow not found in its parent's ChildNodes property.");
+        }
+
+        static string GetXPathToNode(XmlNode node)
+        {
+            if (node.NodeType == XmlNodeType.Attribute)
+            {
+                // attributes have an OwnerElement, not a ParentNode; also they have
+                // to be matched by name, not found by position
+                return String.Format(
+                    "{0}/@{1}",
+                    GetXPathToNode(((XmlAttribute)node).OwnerElement),
+                    node.Name
+                    );
+            }
+            if (node.ParentNode == null)
+            {
+                // the only node with no parent is the root node, which has no path
+                return "";
+            }
+            // the path to a node is the path to its parent, plus "/node()[n]", where 
+            // n is its position among its siblings.
+            return String.Format(
+                "{0}/node()[{1}]",
+                GetXPathToNode(node.ParentNode),
+                GetNodePosition(node)
+                );
         }
     }
 }
